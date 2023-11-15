f040f0d31a85ef343b98acb9e996fd9d151a1fe6
narasimhamadasu <123008253+narasimhamadasu@users.noreply.github.com>
2023-11-14 22:31:41 -0600

    first commit

---
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,15 @@
+//@author Narasimha rajkumar Madasu 
+//SID S555231
+package com.bandiClasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,11 @@
+//@author Narasimha rajkumar Madasu 
+//SID S555231
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
